769406b084abcdfc477d66ee5912c5cdb78dd074
MridulSharma2552007 <mridul25sharma52007@gmail.com>
2025-09-15 23:39:28 +0530

    added nav bar

--- lib/NavBar/navbar.dart
+++ lib/NavBar/navbar.dart
@@ -1,10 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:koru/Colors/app_colors.dart';
 
 class Navbar extends StatelessWidget {
   const Navbar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            border: Border(top: BorderSide(color: Colors.white.withAlpha(3))),
+
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/appscreens/home.dart
+++ lib/appscreens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:koru/NavBar/navbar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
@@ -29,72 +30,92 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, Constraints) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: Constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 100,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
-                              ),
-                              child: Text(
-                                'Hello',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 20,
-                              ),
-                              child: Text(
-                                userName,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+      body: Stack(
+        children: [
+          SafeArea(
+            top: true,
+            bottom: false,
+            child: LayoutBuilder(
+              builder: (context, Constraints) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: Constraints.maxHeight,
+                    ),
+                    child: IntrinsicHeight(
+                      child: Column(
+                        children: [
+                          topwidget(userName: userName),
+                          SizedBox(height: 20),
+                          Expanded(child: homescreenconatiner()),
+                        ],
                       ),
-                      SizedBox(height: 20),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.blueAccent,
-                                const Color.fromARGB(255, 15, 15, 16),
-                              ],
-                              begin: Alignment.topCenter,
-                              end: AlignmentGeometry.bottomCenter,
-                            ),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-            );
-          },
+                );
+              },
+            ),
+          ),
+          Positioned(bottom: 10, left: 10, right: 10, child: Navbar()),
+        ],
+      ),
+    );
+  }
+}
+
+class homescreenconatiner extends StatelessWidget {
+  const homescreenconatiner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [const Color.fromARGB(255, 0, 0, 0), Colors.blueAccent],
+          begin: Alignment.topCenter,
+          end: AlignmentGeometry.bottomCenter,
         ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
+
+class topwidget extends StatelessWidget {
+  const topwidget({super.key, required this.userName});
+
+  final String userName;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'Hello',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              userName,
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
